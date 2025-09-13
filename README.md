e9d7d7fbc46bd21e87a356a5552e06c2baf56de9
panaud.bastien <bastien.panaud@telecom-st-etienne.fr>
2025-09-13 19:22:49 +0200

    Ajout README.md et noms voitures

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # TD_Typescrypt
 TD IHM FISE3 INFO6
+
+Ce projet est une version simplifiée du jeu "Mille Bornes" où 4 voitures font la course jusqu'à atteindre 1000 km en étant confrontées à des évènements aléatoires comme un accident ou un pneu crevé. Le jeu suit les voitures qui avancent à une vitesse aléatoire et affiche les résultats à la fin de chaque tour, il s'arrête lorsqu'une des trois voitures a atteint 1000 km.
+
+Pour lancer le projet :
+
+cd TD_Typescript
+npm install -g typescript
+tsc car.ts event.ts game.ts main.ts
+node main.js
--- main.ts
+++ main.ts
@@ -3,12 +3,14 @@ import { Game } from "./game";
 
 const game = new Game();
 
-const car1 = new Car("Clio", "Renault", "White", 2024);
-const car2 = new Car("308", "Peugeot", "Grey", 2024);
-const car3 = new Car("Focus", "Ford", "Red", 2024);
+const car1 = new Car("Clio IV", "Renault", "Black", 2017);
+const car2 = new Car("A5", "Audi", "Grey", 2020);
+const car3 = new Car("Roma", "Ferrari", "Red", 2023);
+const car4 = new Car("Chiron","Bugatti","Blue",2021)
 
 game.addCar(car1);
 game.addCar(car2);
 game.addCar(car3);
+game.addCar(car4);
 
 game.start();
